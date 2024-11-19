6e61b85a708367f653af203aaca1349d2c653e4c
Khoa Dang <duongtrandangkhoa2511@gmail.com>
2024-11-20 05:48:15 +0700

    staff: CRU staff, define Category, Product

--- src/modules/category/category.schema.ts
+++ src/modules/category/category.schema.ts
@@ -1,5 +1,5 @@
 import { Category } from "../../../prisma/clients/postgres/hostdb";
-
+import { t } from 'elysia';
 export type CategoryType = Category
 export class CategoryObject {
     categoryId?: string
@@ -15,3 +15,34 @@ export class CategoryObject {
         this.hostId = data.hostId
     }
 }
+
+export const CreateCategorySchema = t.Object({
+  categoryName: t.String({ minLength: 1, error: 'Category Name is required' }),
+  hostid: t.String({ format: 'uuid', error: 'Invalid Host ID' }),
+  status: t.Optional(t.Boolean({ error: 'Status must be true or false' })),
+});
+
+
+export const UpdateCategorySchema = t.Object({
+  categoryName: t.String({ minLength: 1, error: 'Category Name is required' }),
+  status: t.Optional(t.Boolean({ error: 'Status must be true or false' })),
+});
+
+export const CategoryParamsSchema = t.Required(
+  t.Object({
+    categoryId: t.String({ format: 'uuid', error: 'Invalid Category ID' }),
+  }),
+   {
+      error: 'Category ID not provived',
+    },
+);
+
+
+export const HostParamsSchema = t.Required(
+  t.Object({
+    hostId: t.String({ format: 'uuid', error: 'Invalid Host ID' }),
+  }),
+   {
+      error: 'Host ID not provived',
+    },
+);
--- src/modules/staff/staff.schema.ts
+++ src/modules/staff/staff.schema.ts
@@ -1,6 +1,7 @@
 import { Staff } from './../../../prisma/clients/index.d';
 
 import { Account, staff } from "../../../prisma/clients/postgres/hostdb";
+import { t } from 'elysia';
 
 export type AccountType = Pick< Account,| 'name' >
 export class VendorAccountType {
@@ -23,3 +24,62 @@ export class StaffObject {
     this.urlQr = data.urlQr;
     }
 }
+export class RegisterVendorObject {
+  username: string
+  password: string
+  name: string 
+  constructor(data: any) {
+    this.username = data.username
+    this.password = data.password
+    this.name = data.name
+  }
+}
+export const RegisterStaff = t.Object({
+  username: t.String({
+    minLength: 8,
+    maxLength: 50,
+    default: '',
+    pattern: '^[A-Za-z][A-Za-z0-9_-]{7,39}$',
+    error:
+      'Username must be between 8 - 40 characters and start with only letters and contain only letters, numbers and underscores',
+  }),
+  password: t.String({
+    default: '',
+    minLength: 8,
+    maxLength: 50,
+    pattern: '^[A-Za-z][.@A-Za-z0-9_-]{7,39}$',
+    error:
+      'Password must be between 8 - 40 characters and start with only letters and contain only letters, numbers and underscores',
+  }),
+  name: t.String({
+    minLength: 1,
+    maxLength: 50,
+    error: 'Name is required',
+  }),
+})
+export const UpdateStaffSchema = t.Object({
+  password: t.String({
+    default: '',
+    minLength: 8,
+    maxLength: 50,
+    pattern: '^[A-Za-z][.@A-Za-z0-9_-]{7,39}$',
+    error:
+      'Password must be between 8 - 40 characters and start with only letters and contain only letters, numbers and underscores',
+  }),
+  name: t.String({
+    minLength: 1,
+    maxLength: 50,
+    error: 'Name is required',
+  }),
+  status: t.Optional(t.Boolean({ error: 'Status must be true or false' })),
+});
+export const GetStaffParams = t.Required(
+  t.Object(
+    {
+      vendorId: t.String({ format: 'uuid', error: 'Invalid Vendor ID' }),  
+    },
+    {
+      error: 'Product ID not provived',
+    },
+  ),
+)
